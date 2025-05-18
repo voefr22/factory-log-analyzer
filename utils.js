0bed030e4ba66fedffc02b81de8437558bcf29e7
voefr22 <voefr27@gmail.com>
2025-05-18 11:24:03 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,35 @@
-
+});
+  
+  const eventTypeData = Object.keys(eventTypeCounts).map(key => ({
+    name: key,
+    value: eventTypeCounts[key]
+  }));
+  
+  // Данные для графика по дням
+  const dateEventCounts = {};
+  
+  data.forEach(entry => {
+    const date = entry.date;
+    if (!dateEventCounts[date]) {
+      dateEventCounts[date] = {
+        date,
+        'Ремонт': 0,
+        'Остановка': 0,
+        'Работа': 0,
+        'Информация': 0,
+        'Подготовка': 0,
+        'Заливка': 0
+      };
+    }
+    
+    dateEventCounts[date][entry.eventType]++;
+  });
+  
+  const timelineData = Object.values(dateEventCounts);
+  
+  return {
+    equipmentStats: equipmentData,
+    eventTypeStats: eventTypeData,
+    timelineData: timelineData
+  };
+}
